1cfa2d67f6b2e44d5b4199590361abca935903a7
akshaysable02 <akshaysable097@gmail.com>
2023-09-06 20:07:28 +0530

    CSS added and create variable to get data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
+  
+  const expenseDate = new Date(2023 , 9 , 6);
+  const expenseTitle = "Buy Book";
+  const expenseAmount= 200;
+  const LocationOfExpenditure = "Akola";
   return (
-    <div>
-      <div>Food  Rs 10</div>
-      <div>Petrol Rs 100</div>
-      <div>Movies Rs 200</div>
+    <div className='expense-item'>
+      <div className=''>{expenseDate.toUTCString()}</div>
+      <div className='expense-item__description '>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}</div>
+        <div>{LocationOfExpenditure}</div>
+      </div>
     </div>
   );
 }
