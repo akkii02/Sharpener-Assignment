a9dbe9b11b36ce67e5811f946897a090eb076060
akshaysable02 <akshaysable097@gmail.com>
2023-09-16 10:14:26 +0530

    on click button change expense amount

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
+import React,{useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [amount,changeAmount] = useState(props.amount);
+
   const clickHandler = ()=>{
-    console.log("Clicked")
+    changeAmount('100$');
+    console.log(amount)
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         title={props.title}
-        amount={props.amount}
+        amount={amount}
         location={props.location}
       />
       <button onClick={clickHandler}>Delete</button>
